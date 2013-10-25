04f46e13a200673fce6eda7ff31b8f4af6178da4
pajanne <pajanne@gmail.com>
2013-10-25 16:58:05 +0000

    update REAME; add python to be checked on setup; fix java installation

--- scripts/chipseq_installer.py
+++ scripts/chipseq_installer.py
@@ -374,7 +374,7 @@ def install_java():
     url = "http://download.oracle.com/otn-pub/java/jdk/7/jdk-7-linux-x64.tar.gz"
     wget_options = '--no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" '
     with lcd(env.tmp_dir):
-        dir_name = _fetch_and_unpack(env.tmp_dir, url)
+        dir_name = _fetch_and_unpack(env.tmp_dir, url, True, wget_options)
     
 
 def install_workflow():
